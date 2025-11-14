d4f776b162c07f6ac77323ed11ad05d9b2df27cd
Vaishnavi Choudhary <chvaishnavi425@gmail.com>
2025-11-14 12:53:09 +0530

    ToDOList

--- day14/todolist/src/Components/InputToDo.jsx
+++ day14/todolist/src/Components/InputToDo.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const InputToDo = () => {
-    const [inputValue, setInputValue] = useState("");
-    return (
-        div
-    )
-}
+const InputToDo = ({ inputHandler }) => {
+  const [inputValue, setInputValue] = useState("");
+  const addTask = () => {
+    if (inputValue.trim() === "") return;
+    inputHandler(inputValue);
+    setInputValue("");
+  };
+  return (
+    <div className="input-todo">
+      <input
+        type="text"
+        name="todo-input"
+        id="todo-input"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTask}>ADD</button>
+    </div>
+  );
+};
+
+export default InputToDo;
